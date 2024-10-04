db654569b82654900d89c5bd52a5a8ed12fc1ce0
Michael Sihotang <sihotangpian@gmail.com>
2024-10-04 18:25:56 +0700

    Generated AppRoute

---
+++ frontend/src/AppRoutes.tsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from "react-router-dom"
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path='/'/>
+    </Routes>
+  )
+}
+
+export default AppRoutes
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,24 @@
+import {BrowserRouter as Router} from 'react-router-dom'
+import {QueryClient, QueryClientProvider} from 'react-query'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import AppRoutes from './AppRoutes.tsx'
 import './index.css'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false
+    }
+  }
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Router>
+      <QueryClientProvider client={queryClient}>
+        <AppRoutes/>
+      </QueryClientProvider>
+    </Router>
   </StrictMode>,
 )
