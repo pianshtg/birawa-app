4f281ff372d088193c8770600c294b57ffab0862
Michael Sihotang <sihotangpian@gmail.com>
2024-10-11 11:01:45 +0700

    Added seeder

--- backend/src/database/index.ts
+++ backend/src/database/index.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config'
 import { createPool } from "mysql2/promise";
 
 export const pool = createPool({
@@ -7,17 +8,14 @@ export const pool = createPool({
     database: process.env.MYSQL_DB,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    multipleStatements: true
 })
 
 export async function testConnection () {
     try {
         const connection = await pool.getConnection()
         console.log("Connected to the database!")
-        // const [testQuery] = await pool.query(
-        //     "select * from city where countrycode = 'IDN';"
-        // )
-        // console.log(testQuery)
         connection.release()
     } catch (err) {
         console.error("Unable to connect to the database:", err)
