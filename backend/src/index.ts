54710693db02244aaf57d5d418aa750604d1105c
Michael Sihotang <sihotangpian@gmail.com>
2024-10-12 11:11:08 +0700

    Added routes and health check

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,37 +1,45 @@
 import express, { Request, Response } from 'express'
-import cors from 'cors'
-import 'dotenv/config'
 import { testConnection } from './database'
-import cookieParser from 'cookie-parser'
 import helmet from 'helmet'
-
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import { csrfProtection } from './middlewares/csurf'
+import authenticationRoute from './routes/AuthenticationRoute'
+import testRoute from './routes/TestRoute'
+import mitraRoute from './routes/MitraRoute'
+import userRoute from './routes/UserRoute'
 const app = express()
 
-// Middleware
+// Middlewares
 app.use(helmet())
 app.use(express.json())
-app.use(cors({
-    origin:'http://localhost:5173',
-    credentials: true
-}))
+// app.use(cors({
+//     origin:'http://localhost:5173',
+//     credentials: true
+// }))
 app.use(cookieParser())
 // app.use(csrfProtection)
 
+// Health Check
 app.get("/", async (req: Request, res: Response) => {
-    res.json({message: 'Hello!'})
+    res.json({message: "Hi!"})
 })
 
-// Routes
+//Routes
+app.use("/api/auth", authenticationRoute)
+app.use("/api/test", testRoute)
+app.use("/api/mitra", mitraRoute)
+app.use("/api/user", userRoute)
 
 // Start Server
 async function startServer() {
     try {
         await testConnection()
         app.listen(3030, () => {
-            console.log("Server started on localhost:3030!")
+            console.log("Server started on localhost:3030..")
         })
-    } catch (err) {
-        console.error("Failed to connect database. Server is failed to start.", err)
+    } catch (error) {
+        console.error("Failed to connect database. Server is failed to start: ", error)
     }
 }
 
