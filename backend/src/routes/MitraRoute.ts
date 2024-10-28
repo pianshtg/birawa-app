c06145931e5847456d8c6d3dc27630745d668d0c
Michael Sihotang <sihotangpian@gmail.com>
2024-10-28 21:22:35 +0700

    feat: add middleware to mitra route

--- backend/src/routes/MitraRoute.ts
+++ backend/src/routes/MitraRoute.ts
@@ -1,9 +1,10 @@
 import express from 'express'
 import MitraController from '../controllers/MitraController'
+import { clientType, jwtCheck } from '../middlewares/auth'
 
 const router = express.Router()
 
-router.post('/', MitraController.createMitra)
+router.post('/', clientType, jwtCheck, MitraController.createMitra)
 router.get('/', MitraController.getMitra)
 router.patch('/', MitraController.updateMitra)
 router.delete('/', MitraController.deleteMitra)
