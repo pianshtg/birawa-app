fe71e8b36fb6a99d55df72045a6bc2df467a9dce
Michael Sihotang <sihotangpian@gmail.com>
2024-11-06 18:01:47 +0700

    feat: add request validation as middleware to routes

--- backend/src/routes/PekerjaanRoute.ts
+++ backend/src/routes/PekerjaanRoute.ts
@@ -1,9 +1,10 @@
 import express from 'express'
 import PekerjaanController from '../controllers/PekerjaanController'
+import { validatePekerjaanRequest } from '../middlewares/validation'
 
 const router = express.Router()
 
-router.post('/', PekerjaanController.createPekerjaan)
+router.post('/', validatePekerjaanRequest, PekerjaanController.createPekerjaan)
 router.get('/', PekerjaanController.getPekerjaan)
 router.patch('/', PekerjaanController.updatePekerjaan)
 router.delete('/', PekerjaanController.deletePekerjaan)
