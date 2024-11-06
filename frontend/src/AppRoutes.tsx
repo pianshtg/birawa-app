601d473ed8a03209729226a4ee5f9b0456c60e26
JeckPGR <dzakyrazi@gmail.com>
2024-11-06 18:04:28 +0700

    refactor codebase agar useDetection ter USE di semua routes

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -5,20 +5,36 @@ import Ceklaporan from "./pages/CPM/ceklaporan";
 import Inbox from "./pages/inbox";
 import Buatlaporan from "./pages/Mitra/buatlaporan";
 import NotFound from "./pages/notfound";
-import Profile from "./pages/profile";
+import Profile from "./pages/settings";
 import Forgotpassword from "./pages/forgotpassword";
-import ResponsiveLayout from "./pages/responsivelayout";
+import Layout from "./components/custom/layout/Layout";
+import SettingsPage from "./pages/settings";
+import MobilePage from "./pages/mobilepage"; // Halaman khusus mobile
+import useDetection from "@/hooks/useDetection"; // Import custom hook
 
 function AppRoutes() {
+  const isMobile = useDetection(620); // Tentukan ukuran maksimum untuk dianggap sebagai mobile
+
+  // Jika perangkat adalah mobile, arahkan ke halaman khusus mobile
+  if (isMobile) {
+    return (
+      <Routes>
+        <Route path="*" element={<MobilePage />} />
+      </Routes>
+    );
+  }
+
+  // Jika bukan perangkat mobile, tampilkan layout aplikasi penuh
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path="/dashboard" element={<ResponsiveLayout><Dashboard /></ResponsiveLayout>} />
-      <Route path='/ceklaporan' element={<ResponsiveLayout><Ceklaporan /></ResponsiveLayout>} />
-      <Route path='/inbox' element={<ResponsiveLayout><Inbox /></ResponsiveLayout>} />
-      <Route path='/buatlaporan' element={<ResponsiveLayout><Buatlaporan /></ResponsiveLayout>} />
-      <Route path='/profile' element={<ResponsiveLayout><Profile /></ResponsiveLayout>} />
+      <Route path="/dashboard" element={<Layout><Dashboard /></Layout>} />
+      <Route path='/ceklaporan' element={<Layout><Ceklaporan /></Layout>} />
+      <Route path='/inbox' element={<Layout><Inbox /></Layout>} />
+      <Route path='/buatlaporan' element={<Layout><Buatlaporan /></Layout>} />
+      <Route path='/profile' element={<Layout><Profile /></Layout>} />
       <Route path='/forgotpassword' element={<Forgotpassword />} />
+      <Route path='/settings/*' element={<SettingsPage />} />
 
       {/* 404 Not Found Page */}
       <Route path='*' element={<NotFound />} />
