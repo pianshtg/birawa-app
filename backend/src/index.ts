52ac061c8b712ea2c5bee0a9e78a3f97a2ca1f66
Michael Sihotang <sihotangpian@gmail.com>
2024-11-06 18:09:06 +0700

    chore: activate cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -26,10 +26,10 @@ cloudinary.config({
 // Middlewares
 app.use(helmet())
 app.use(express.json())
-// app.use(cors({
-//     origin:'http://localhost:5173',
-//     credentials: true
-// }))
+app.use(cors({
+    origin:['http://localhost:5173'],
+    credentials: true
+}))
 app.use(cookieParser())
 // app.use(csrfProtection)
 
