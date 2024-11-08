acbd2c0a8b9bdc6f7942a4b6e25c9074d58f6563
Michael Sihotang <sihotangpian@gmail.com>
2024-11-08 12:54:38 +0700

    feat: add loading button component

---
+++ frontend/src/components/LoadingButton.tsx
@@ -0,0 +1,18 @@
+import { Button } from './ui/button'
+import { Loader2 } from 'lucide-react'
+
+const LoadingButton = () => {
+  return (
+      <div className="relative group w-[25%] ml-1 mt-2 h-auto">
+        <Button disabled className='relative w-full top-[1px] left-[-1px] z-[1]'>
+          <Loader2 className='h-4 w-4 animate-spin'>
+              Loading...
+          </Loader2>
+        </Button>
+        {/* BACKGROUND */}
+        <div className="flex absolute top-[3px] left-[-3px] w-full h-full md:bg-black md:rounded-md z-[0]"/>
+      </div>
+  )
+}
+
+export default LoadingButton
