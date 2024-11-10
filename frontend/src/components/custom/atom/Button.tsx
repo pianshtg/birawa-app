076738b486b8629d572ae7f97f5acbf88e143ea3
Fathinzhy <ghazyfathin21@gmail.com>
2024-11-10 13:21:26 +0700

    feat: custom Button

--- frontend/src/components/custom/atom/Button.tsx
+++ frontend/src/components/custom/atom/Button.tsx
@@ -1,16 +1,18 @@
+// Button.tsx
 import React from 'react';
 
 interface ButtonProps {
   type: "button" | "submit" | "reset";
   onClick?: () => void;
   children: React.ReactNode;
+  className?: string; 
 }
 
-const Button: React.FC<ButtonProps> = ({ type, onClick, children }) => (
+const Button: React.FC<ButtonProps> = ({ type, onClick, children, className }) => (
   <button
     type={type}
     onClick={onClick}
-    className="bg-primary w-full hover:bg-primary ease-in-out duration-150 text-white font-semibold py-2 px-4 rounded focus:outline-none"
+    className={`${className} bg-primary w-full  hover:bg-primary ease-in-out duration-150  font-semibold py-2 px-4 rounded focus:outline-none `}
   >
     {children}
   </button>
