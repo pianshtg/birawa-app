1c305325956ec0250d62943287fe35ad0568988e
Michael Sihotang <sihotangpian@gmail.com>
2024-11-18 13:36:25 +0700

    feat: add authenticate user controller & route

--- backend/src/controllers/AuthenticationController.ts
+++ backend/src/controllers/AuthenticationController.ts
@@ -118,6 +118,84 @@ async function loginUser (req: Request, res: Response) {
     }
 }
 
+async function authenticateUser(req: Request, res: Response) {
+    console.log("Jwt checking...") //Debug.
+    try {
+        // console.log('Received cookies (jwtCheck):', req.cookies) // Debug.
+        const accessToken = req.accessToken
+        // console.log(token) (debuggin)
+        if (accessToken && jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET_KEY as string)) {
+            // console.log(token) // Debug.
+            console.log('Successfully authenticating jwt.') //Debug.
+            res.status(201).json({message: "User successfully authenticated."})
+            return
+        } else {
+            // console.log("there's no access token") // Debug.
+            res.status(401).json({message: "Unauthorized"})
+            return
+        }
+    } catch (error) {
+        console.log("Access token verification failed:", error) // Debug.
+
+        if (error instanceof jwt.TokenExpiredError) {
+            const refreshToken = req.refreshToken!
+            try {
+                // Decode the refresh token
+                const decodedRefreshToken = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET_KEY as string) as jwt.JwtPayload
+
+                // Check if the user has refresh token in the database
+                const [user_hashed_refresh_token] = await pool.execute<RowDataPacket[]>('SELECT hashed_refresh_token FROM users_hashed_refresh_token WHERE user_id = ?', [decodedRefreshToken.user_id])
+                if (user_hashed_refresh_token.length === 0) {
+                    res.status(401).json({message: "Unauthorized."})
+                    return
+                }
+
+                // Check if the refresh token is valid
+                const isValidRefreshToken = await bcrypt.compare(refreshToken, user_hashed_refresh_token[0].hashed_refresh_token)
+                if (isValidRefreshToken) {
+                    // Generate new access token since the refresh token is valid
+                        // Transferring the data from refresh token to the new access token
+                    const user_id = decodedRefreshToken.user_id
+                    const permissions = decodedRefreshToken.permissions
+                    const newAccessToken = generateAccessToken({user_id, permissions})
+                    
+                    console.log("Successfuly renewed the accessToken: ", newAccessToken, "\n") // Debug.
+                    
+                    // Directly set the new access token to the cookie jar if the request comes from web
+                    const clientType = req.headers['x-client-type']
+                    if (clientType === 'web') {
+                        res.cookie('accessToken', newAccessToken, {
+                            httpOnly: true,
+                            secure: true,
+                            sameSite: 'none',
+                            maxAge: 15 * 60 * 1000,
+                            path: '/'
+                        })
+                        return
+                    } else {
+                        // Passing the new access token to user
+                        res.status(201).json({
+                            message: "User successfully authenticated.",
+                            newAccessToken
+                        })
+                        return
+                    }
+                } else {
+                    res.status(401).json({ message: "Invalid refresh token." });
+                    return
+                }
+            } catch (error) {
+                console.error(error) // Debug.
+                res.status(401).json({message: "Unauthorized"})
+                return
+            }
+        } else {
+            res.status(401).json({message: "Unauthorized."})
+            return
+        }
+    }
+}
+
 async function verifyEmail(req: Request, res: Response) {
     const {token} = req.query
     try {
@@ -142,5 +220,6 @@ async function verifyEmail(req: Request, res: Response) {
 
 export default {
     loginUser,
+    authenticateUser,
     verifyEmail
 }
--- backend/src/routes/AuthenticationRoute.ts
+++ backend/src/routes/AuthenticationRoute.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import AuthenticationController from '../controllers/AuthenticationController'
 import { csrfToken } from '../middlewares/csurf'
+import { clientType } from '../middlewares/auth'
 
 const router = express.Router()
 
+router.post('/', clientType, AuthenticationController.authenticateUser)
 router.post('/signin', AuthenticationController.loginUser)
 router.get('/verify-email', AuthenticationController.verifyEmail)
 router.get('/csrf-token', csrfToken)
