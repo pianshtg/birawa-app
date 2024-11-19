35224e2734820a3803cbddc5038d56515424b105
Michael Sihotang <sihotangpian@gmail.com>
2024-11-19 11:15:16 +0700

    hotfix: fix create laporan api request logic

--- frontend/src/api/LaporanApi.ts
+++ frontend/src/api/LaporanApi.ts
@@ -1,23 +1,36 @@
-import { useMutation } from "react-query";
+import { Aktivitas, Shift, TenagaKerja } from "@/types";
+import { useMutation, useQuery } from "react-query";
 import { toast } from "sonner";
 
-const API_BASE_URL = import.meta.env.VITE_BASE_API_URL
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 type CreateLaporanRequest = {
-    
+    nama_mitra: string,
+    nomor_kontrak: string,
+    nama_pekerjaan: string,
+    tanggal: string,
+    shift: Shift,
+    tenaga_kerja_arr: TenagaKerja[],
+    aktivitas_arr: Aktivitas[]
 };
 
-export function useCreateLaporan () {
-    async function useCreateLaporanRequest (laporan: CreateLaporanRequest) {
+export function useCreateLaporan() {
+    async function useCreateLaporanRequest (laporan: CreateLaporanRequest, files: File[]) {
+        const formData = new FormData()
+        formData.append('data', JSON.stringify(laporan))
+        files.forEach((file, index) => {
+            formData.append(`files[${index}]`, file)
+        })
+        
+        
         // const csrfToken = await getCsrfToken() // Hasn't implemented csrf token yet.
         const response = await fetch(`${API_BASE_URL}/api/laporan`, {
-            method: 'GET',
+            method: 'POST',
             headers: {
-                "Content-Type": "application/json",
                 "X-Client-Type": "web"
                 // "X-CSRF-TOKEN": csrfToken // Hasn't implemented csrf token yet.
             },
-            body: JSON.stringify(laporan),
+            body: formData,
             credentials: 'include'
         })
         if (!response.ok) {
@@ -33,10 +46,10 @@ export function useCreateLaporan () {
         isSuccess,
         error,
         reset
-    } = useMutation(useCreateLaporanRequest)
+    } = useMutation(({laporan, files}: {laporan: CreateLaporanRequest, files: File[]}) => useCreateLaporanRequest(laporan, files))
 
     if (isSuccess) {
-        toast.success("Laporan Berhasil Dibuat!")
+        toast.success("Laporan berhasil dibuat!")
     }
 
     if (error) {
