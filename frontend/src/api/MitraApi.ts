d4d0196a778da63f02962f9aee7e1b793116f9e8
Michael Sihotang <sihotangpian@gmail.com>
2024-11-19 17:39:44 +0700

    chore: readability

--- frontend/src/api/MitraApi.ts
+++ frontend/src/api/MitraApi.ts
@@ -7,7 +7,7 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 type CreateMitraRequest = {
     mitra: Mitra,
     kontrak: Kontrak,
-    pekerjaan: Pekerjaan[],
+    pekerjaan_arr: Pekerjaan[],
     user: User
 };
 
