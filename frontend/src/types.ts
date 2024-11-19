26e0700fc7ddba0b90f1d7627275e59f8ebb50f0
Michael Sihotang <sihotangpian@gmail.com>
2024-11-19 11:20:23 +0700

    feat: add types

---
+++ frontend/src/types.ts
@@ -0,0 +1,46 @@
+export type Mitra = {
+    nama: string,
+    nomor_telepon: string,
+    alamat: string
+}
+
+export type Kontrak = {
+    nama: string,
+    nomor: string,
+    tanggal: string,
+    nilai: number,
+    jangka_waktu: number
+}
+
+export type Pekerjaan = {
+    nama: string,
+    lokasi: string
+}
+
+export type User = {
+    email: string,
+    nama_lengkap: string,
+    nomor_telepon: string
+}
+
+export type Shift = {
+    nama: string,
+    waktu_mulai: string,
+    waktu_berakhir: string
+}
+
+export type TenagaKerja = {
+    tipe: string,
+    peran: string,
+    jumlah: number
+}
+
+export type Dokumentasi = {
+    deskripsi: string
+}
+
+export type Aktivitas = {
+    tipe: string,
+    nama: string,
+    dokumentasi: Dokumentasi[]
+}
