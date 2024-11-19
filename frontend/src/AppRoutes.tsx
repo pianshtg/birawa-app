830c0b7ab2f596532bf92a18536eecf85b2f8f61
Michael Sihotang <sihotangpian@gmail.com>
2024-11-19 11:19:37 +0700

    feat: add protected route

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,17 +1,18 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/LoginPage";
-import Dashboard from "./pages/DashboardPage";
-import Ceklaporan from "./pages/CPM/CekLaporanPage";
-import Inbox from "./pages/InboxPage";
-import Buatlaporan from "./pages/Mitra/buatlaporan/BuatLaporanPage";
-import NotFound from "./pages/NotFoundPage";
-import Forgotpassword from "./pages/ForgotPasswordPage";
-import Layout from "./components/custom/layout/Layout";
+import Home from "@/pages/LoginPage";
+import Dashboard from "@/pages/DashboardPage";
+import Ceklaporan from "@/pages/CPM/CekLaporanPage";
+import Inbox from "@/pages/InboxPage";
+import Buatlaporan from "@/pages/Mitra/buatlaporan/BuatLaporanPage";
+import NotFound from "@/pages/NotFoundPage";
+import Forgotpassword from "@/pages/ForgotPasswordPage";
+import Layout from "@/components/custom/layout/Layout";
 import SettingsPage from "@/pages/SettingsPage";
-import MobilePage from "./pages/mobilepage";
+import MobilePage from "@/pages/MobilePage"; // Halaman khusus mobile
 import useDetection from "@/hooks/useDetection"; // Import custom hook
 import DaftarPekerjaan from "@/pages/Mitra/buatlaporan/DaftarPekerjaanPage";
-import TambahMitraPage from "./pages/CPM/TambahMitraPage";
+import TambahMitraPage from "@/pages/CPM/TambahMitraPage";
+import ProtectedRoute from "@/auth/ProtectedRoute";
 
 function AppRoutes() {
   const isMobile = useDetection(620); // Tentukan ukuran maksimum untuk dianggap sebagai mobile
@@ -30,22 +31,65 @@ function AppRoutes() {
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/forgotpassword' element={<Forgotpassword />} />
+      
+      <Route element={<ProtectedRoute/>}>
+        <Route path="/dashboard" 
+          element={
+            <Layout>
+              <Dashboard />
+            </Layout> 
+          }
+        />
+        <Route path="/tambahmitra" 
+          element={
+            <Layout>
+              <TambahMitraPage />
+            </Layout>
+          } 
+        />
 
-      <Route path="/dashboard" element={<Layout><Dashboard /></Layout>} />
-      <Route path="/dashboard/tambahmitra" element={<Layout><TambahMitraPage /></Layout>} />
+        {/* Buat Laporan Page */}
+        <Route path='/daftarpekerjaan' 
+          element={
+            <Layout>
+              <DaftarPekerjaan/>
+            </Layout>
+          } 
+        /> 
+        <Route path='/daftarpekerjaan/buatlaporan' 
+          element={
+            <Layout>
+              <Buatlaporan/>
+            </Layout>
+          } 
+        /> 
+        <Route path='/daftarpekerjaan/buatlaporan/ko' 
+          element={
+            <Layout>
+              <Buatlaporan/>
+            </Layout>
+          } 
+        /> 
 
-      {/* Buat Laporan Page */}
-      <Route path='/daftarpekerjaan' element={<Layout> <DaftarPekerjaan/></Layout>} /> 
-      <Route path='/daftarpekerjaan/buatlaporan' element={<Layout> <Buatlaporan/></Layout>} /> 
-      <Route path='/daftarpekerjaan/buatlaporan/ko' element={<Layout> <Buatlaporan/></Layout>} /> 
+        <Route path='/ceklaporan' 
+          element={
+            <Layout>
+              <Ceklaporan />
+            </Layout>
+          } 
+        />
 
-      <Route path='/ceklaporan' element={<Layout><Ceklaporan /></Layout>} />
-
-      <Route path='/inbox' element={<Layout><Inbox /></Layout>} />
-     
-      <Route path='/settings/*' element={<SettingsPage />} />
-    
-
+        <Route path='/inbox' 
+          element={
+            <Layout>
+              <Inbox />
+            </Layout>
+          } 
+        />
+      
+        <Route path='/settings/*' element={<SettingsPage />} />
+        
+      </Route>
       {/* 404 Not Found Page */}
       <Route path='*' element={<NotFound />} />
     </Routes>
