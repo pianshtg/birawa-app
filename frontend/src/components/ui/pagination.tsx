9dfdbb6d9ef6904dd7c4af33b8df946e7a78edcc
Michael Sihotang <sihotangpian@gmail.com>
2024-11-21 14:10:47 +0700

    feat: add components

--- frontend/src/components/ui/pagination.tsx
+++ frontend/src/components/ui/pagination.tsx
@@ -65,7 +65,7 @@ const PaginationPrevious = ({
   <PaginationLink
     aria-label="Go to previous page"
     size="default"
-    className={cn("gap-1 pl-2.5", className)}
+    className={cn("gap-1 pl-2.5 cursor-pointer", className)}
     {...props}
   >
     <ChevronLeftIcon className="h-4 w-4" />
@@ -81,7 +81,7 @@ const PaginationNext = ({
   <PaginationLink
     aria-label="Go to next page"
     size="default"
-    className={cn("gap-1 pr-2.5", className)}
+    className={cn("gap-1 pr-2.5 cursor-pointer", className)}
     {...props}
   >
     <span>Selanjutnya</span>
