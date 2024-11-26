292b92f716c8c7c010f42bbeb7a3208504d4058c
Michael Sihotang <sihotangpian@gmail.com>
2024-11-27 00:17:16 +0700

    feat: add cookie fetcher

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -4,3 +4,17 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getCookies (cookieName: string) {
+  const cookiesJar = `; ${document.cookie}`
+  const cookieFilter = cookiesJar.split(`; ${cookieName}=`)[1]
+  if (cookieFilter) {
+    const cookie = cookieFilter.split(';').shift()
+    console.log(`Cookie (${cookieName})`, cookie) //Debug.
+    return cookie
+  }
+}
+
+export function getAccessToken () {
+  return getCookies ('accessToken')
+}
