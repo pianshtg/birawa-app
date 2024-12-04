115f12092b429f27aff34fdb2569d83ee0071119
Michael Sihotang <sihotangpian@gmail.com>
2024-12-04 22:39:48 +0700

    refactor: shadow container refactors

--- frontend/src/components/custom/atom/ShadowContainer.tsx
+++ frontend/src/components/custom/atom/ShadowContainer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from "react";
 import {
   Dialog,
   DialogContent,
@@ -6,44 +6,60 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
-
+} from "@/components/ui/dialog";
 
 interface ShadowProps {
   title?: string;
   buttonName?: string;
-  buttonNeeded? : boolean;
-  titleNeeded? :boolean;
-  Dialogtitle? : string;
-  DialogDesc?:React.ReactNode;
+  buttonNeeded?: boolean;
+  titleNeeded?: boolean;
+  dialogTitle?: string;
+  dialogDescription?: string;
+  dialogContent?: React.ReactNode;
   children: React.ReactNode;
-  onClick?: () => void; // Correct type for an onClick function
+  isDialogOpen?: boolean;
+  setIsDialogOpen?: React.Dispatch<React.SetStateAction<boolean>>;
+  className?: string;
 }
 
-export const ShadowContainer: React.FC<ShadowProps> = ({ title, children, buttonName, buttonNeeded =true, titleNeeded=true, onClick ,Dialogtitle,DialogDesc }) => {
+export const ShadowContainer: React.FC<ShadowProps> = ({
+  title,
+  children,
+  buttonName,
+  buttonNeeded = true,
+  titleNeeded = true,
+  dialogTitle,
+  dialogDescription,
+  dialogContent,
+  isDialogOpen,
+  setIsDialogOpen,
+  className,
+}) => {
+  const [isLocalDialogOpen, setIsLocalDialogOpen] = useState(false);
+
+  const dialogOpen = isDialogOpen ?? isLocalDialogOpen;
+  const setDialogOpen = setIsDialogOpen ?? setIsLocalDialogOpen;
+
   return (
-    <div className="m-3 border rounded-md">
-      <div className={`flex px-4  justify-between  w-full ${titleNeeded ? "border-b py-2" : ""}`}>
+    <div className={`w-full border rounded-md ${className || ''}`}>
+      <div className={`flex px-4 justify-between w-full h-auto ${titleNeeded ? "border-b py-2" : ""}`}>
         <h3 className="text-lg font-semibold">{title}</h3>
-         {buttonNeeded ? 
-           <Dialog>
-           <DialogTrigger className="hover:text-primary duration-200 ease-in-out flex items-center gap-x-2 text-xs font-semibold">{buttonName} </DialogTrigger>
-           <DialogContent>
-             <DialogHeader>
-               <DialogTitle>{Dialogtitle}</DialogTitle>
-               <DialogDescription>
-                {DialogDesc}
-               </DialogDescription>
-             </DialogHeader>
-           </DialogContent>
-         </Dialog>
-          :
-          <></>
-        }
+        {buttonNeeded && (
+          <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
+            <DialogTrigger className="hover:text-primary duration-200 ease-in-out flex items-center gap-x-2 text-xs font-semibold">
+              {buttonName}
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>{dialogTitle}</DialogTitle>
+                <DialogDescription>{dialogDescription}</DialogDescription>
+              </DialogHeader>
+              {dialogContent}
+            </DialogContent>
+          </Dialog>
+        )}
       </div>
-      <div className="p-4">
-        {children}
-      </div>
+      <div className="p-4">{children}</div>
     </div>
   );
 };
