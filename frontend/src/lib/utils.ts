6fa2e9174cfa2d15e163bcb4dd2755041d2fb992
Michael Sihotang <sihotangpian@gmail.com>
2024-12-08 13:01:35 +0700

    feat: add format date and initials utilities

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -45,3 +45,37 @@ export const formatMinutesToTime = (minutes: number) => {
   const mins = (minutes % 60).toString().padStart(2, "0")
   return `${hours}:${mins}`
 }
+
+export function formatDate(inputDate: string): string {
+  const days = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"];
+  const months = [
+      "Jan", "Feb", "Mar", "Apr", "Mei", "Jun",
+      "Jul", "Agu", "Sep", "Okt", "Nov", "Des"
+  ];
+
+  // Parse the input date
+  const date = new Date(inputDate);
+
+  // Extract date components
+  const dayName = days[date.getDay()];
+  const day = date.getDate();
+  const month = months[date.getMonth()];
+  const year = date.getFullYear();
+
+  // Format the time components
+  let hours = date.getHours();
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const ampm = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12 || 12; // Convert to 12-hour format
+
+  return `${dayName}, ${day} ${month} ${year} ${hours}:${minutes} ${ampm}`;
+}
+
+export function formatMitraInitials(name: string) {
+  const nameParts = name.split(' ');  // Pisahkan nama berdasarkan spasi
+  const initials = nameParts
+    .map(part => part.charAt(0).toUpperCase())  // Ambil huruf pertama dari setiap kata
+    .join('');  // Gabungkan menjadi satu string
+
+  return initials.substring(0, 1)+initials.substring(initials.length-1, initials.length);  // Ambil tiga huruf pertama
+}
