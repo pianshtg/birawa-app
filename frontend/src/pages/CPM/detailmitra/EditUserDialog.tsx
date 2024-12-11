11ad8ee7102dd01b6459ed78e65da1a5e64f0910
Michael Sihotang <sihotangpian@gmail.com>
2024-12-11 12:34:49 +0700

    hotfix: fix edit user dialog

--- frontend/src/pages/CPM/detailmitra/EditUserDialog.tsx
+++ frontend/src/pages/CPM/detailmitra/EditUserDialog.tsx
@@ -11,29 +11,27 @@ import { getCountries, getCountryCallingCode } from "libphonenumber-js"
 import { Country } from '@/types'
 import { ChevronDown } from "lucide-react"
 import { User } from '@/types';
+import LoadingButton from '@/components/LoadingButton';
 
-const phoneNumberSchema = z.string()
-  .min(4, "Nomor telepon terlalu pendek")
-  .max(16, "Nomor telepon terlalu panjang")
-  .regex(/^\+?\d+$/, "Nomor telepon hanya boleh berisi angka dan tanda '+'");
 
 const formEditUserSchema = z.object({
   nama_lengkap: z.string().min(1, "Nama pengguna wajib diisi").max(40, "Nama pengguna terlalu panjang").optional(),
   email: z.string().email("Format email tidak valid").min(1, "Email pengguna wajib diisi").optional(),
+  nomor_telepon: z.string().min(4,"Nomor Telephone terlalu sedikit").max(16, "Nomor Telephone melebih batas"),
   // is_active: z.boolean(),
-  nomor_telepon: phoneNumberSchema.optional(),
 });
 
 export type EditUserSchema = z.infer<typeof formEditUserSchema>;
 
-interface EditUserDialogProps {
+type Props = {
   isOpen: boolean;
   onClose: () => void;
   onSubmit: (data: EditUserSchema) => void;
   user: User | null;  // Accept user data as prop
+  isLoading: boolean
 }
 
-const EditUserDialog: React.FC<EditUserDialogProps> = ({ isOpen, onClose, onSubmit, user }) => {
+const EditUserDialog = ({ isOpen, onClose, onSubmit, user, isLoading }: Props) => {
   // Initialize the form with user data, always call useForm
   const form = useForm<EditUserSchema>({
     resolver: zodResolver(formEditUserSchema),
@@ -57,32 +55,42 @@ const EditUserDialog: React.FC<EditUserDialogProps> = ({ isOpen, onClose, onSubm
           {user ? (
             <Form {...form}>
               <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
-                <FormField control={form.control} name="nama_lengkap" render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Nama Pengguna</FormLabel>
-                    <FormControl>
-                      <Input 
-                      {...field} 
-                      placeholder="Masukkan Nama Pengguna"
-                      defaultValue={user.nama_lengkap}/>
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )} />
-                
-                <FormField control={form.control} name="email" render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Email Pengguna</FormLabel>
-                    <FormControl>
-                      <Input 
+                <FormField 
+                  control={form.control} 
+                  name="nama_lengkap" 
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Nama Pengguna</FormLabel>
+                      <FormControl>
+                        <Input 
                         {...field} 
-                        type="email" 
-                        placeholder="Masukkan Email Pengguna" 
-                        defaultValue={user.email}/>
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )} />
+                        placeholder="Masukkan Nama Pengguna"
+                        defaultValue={user.nama_lengkap}/>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )} 
+                />
+                
+                <FormField 
+                  control={form.control} 
+                  name="email" 
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email Pengguna</FormLabel>
+                      <FormControl>
+                        <Input 
+                          {...field} 
+                          type="email" 
+                          placeholder="Masukkan Email Pengguna" 
+                          defaultValue={user.email}
+                          disabled={true}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )} 
+                />
 
                 {/* <FormField control={form.control} name="is_active" render={({ field }) => (
                   <FormItem>
@@ -120,93 +128,111 @@ const EditUserDialog: React.FC<EditUserDialogProps> = ({ isOpen, onClose, onSubm
 
                 
                 <FormField
-                control={form.control}
-                name="nomor_telepon"
-                render={({ field }) => {
-                  const [localPhoneNumber, setLocalPhoneNumber] = useState("");
-                  const [selectedDialCode, setSelectedDialCode] = useState("+62"); // Default to Indonesia
-                  const [searchTerm, setSearchTerm] = useState(""); // State for search term
-                  const [filteredCountries, setFilteredCountries] = useState(countries); // Filtered countries list
-                  
-                  useEffect(() => {
-                    field.onChange(`${selectedDialCode}${localPhoneNumber}`);
-                  }, [selectedDialCode, localPhoneNumber, field]);
-                  
-                  // Filter countries based on search term
-                  useEffect(() => {
-                    setFilteredCountries(
-                      countries.filter((country) =>
-                        country.name.toLowerCase().includes(searchTerm.toLowerCase())
-                      )
-                    );
-                  }, [searchTerm, countries]);
-                  
-                  return (
-                    <FormItem>
-                      <FormLabel>Nomor Telepon User</FormLabel>
-                      <FormControl>
-                        <div className="flex items-start gap-2">
-                          {/* Dropdown for country code */}
-                          <DropdownMenu>
-                            <DropdownMenuTrigger asChild>
-                              <Button
-                                className="w-1/9 flex items-center gap-2 border rounded-md px-3 py-2 bg-gray-100 text-black hover:bg-gray-200"
-                                variant="outline"
+                  control={form.control}
+                  name="nomor_telepon"
+                  render={({ field }) => {
+                    // Extract the selectedDialCode and localPhoneNumber from the user data
+                    const userPhoneNumber = user?.nomor_telepon || '';
+                    const initialDialCode = countries.find(country => userPhoneNumber.startsWith(country.dialCode))?.dialCode || '+62';
+                    const initialLocalPhoneNumber = userPhoneNumber.replace(initialDialCode, '');
+
+                    // States for phone number management
+                    const [localPhoneNumber, setLocalPhoneNumber] = useState(initialLocalPhoneNumber);
+                    const [selectedDialCode, setSelectedDialCode] = useState(initialDialCode);
+                    const [searchTerm, setSearchTerm] = useState(''); // State for search term
+                    const [filteredCountries, setFilteredCountries] = useState(countries); // Filtered countries list
+
+                    useEffect(() => {
+                      // Combine dial code and local phone number to set the field value
+                      const correctedPhoneNumber = localPhoneNumber.startsWith('0')
+                        ? localPhoneNumber.slice(1)
+                        : localPhoneNumber;
+                      field.onChange(`${selectedDialCode}${correctedPhoneNumber}`);
+                    }, [selectedDialCode, localPhoneNumber, field]);
+
+                    // Filter countries based on search term
+                    useEffect(() => {
+                      setFilteredCountries(
+                        countries.filter((country) =>
+                          country.name.toLowerCase().includes(searchTerm.toLowerCase())
+                        )
+                      );
+                    }, [searchTerm]);
+
+                    return (
+                      <FormItem>
+                        <FormLabel>Nomor Telepon User</FormLabel>
+                        <FormControl>
+                          <div className="flex items-start gap-2">
+                            {/* Dropdown for country code */}
+                            <DropdownMenu>
+                              <DropdownMenuTrigger asChild>
+                                <Button
+                                  className="w-1/9 flex items-center gap-2 border rounded-md px-3 py-2 bg-gray-100 text-black hover:bg-gray-200"
+                                  variant="outline"
+                                >
+                                  {selectedDialCode}
+                                  <ChevronDown className="w-4 h-4" />
+                                </Button>
+                              </DropdownMenuTrigger>
+                              <DropdownMenuContent
+                                side="bottom"
+                                align="start"
+                                className="w-64 max-h-60 overflow-y-auto shadow-lg"
                               >
-                                {selectedDialCode}
-                                <ChevronDown className="w-4 h-4" />
-                              </Button>
-                            </DropdownMenuTrigger>
-                            <DropdownMenuContent side='bottom' align='start' className="w-64 max-h-60 overflow-y-auto shadow-lg">
-                              <div className="p-2">
-                                <Input
-                                  className="w-full mb-2"
-                                  placeholder="Search country..."
-                                  onChange={(e) => setSearchTerm(e.target.value)}
-                                  defaultValue={user.nomor_telepon}
-                                />
-                              </div>
-                              {filteredCountries.length > 0 ? (
-                                filteredCountries.map((country) => (
-                                  <DropdownMenuItem
-                                    className="flex justify-between text-sm cursor-pointer"
-                                    key={country.code}
-                                    onClick={() => setSelectedDialCode(country.dialCode)}
-                                  >
-                                    <span>{country.name}</span>
-                                    <span className="text-gray-500">{country.dialCode}</span>
-                                  </DropdownMenuItem>
-                                ))
-                              ) : (
-                                <div className="text-center text-sm text-gray-500 p-2">
-                                  No countries found
+                                <div className="p-2">
+                                  <Input
+                                    className="w-full mb-2"
+                                    placeholder="Search country..."
+                                    onChange={(e) => setSearchTerm(e.target.value)}
+                                  />
                                 </div>
-                              )}
-                            </DropdownMenuContent>
-                          </DropdownMenu>
-                
-                          {/* Input field for phone number */}
-                          <Input
-                            className="flex-1 border rounded-md p-2"
-                            defaultValue={user.nomor_telepon}
-                            onChange={(e) => setLocalPhoneNumber(e.target.value)}
-                            placeholder="Nomor Telepon"
-                          />
-                        </div>
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )
-                }}
-              /> 
+                                {filteredCountries.length > 0 ? (
+                                  filteredCountries.map((country) => (
+                                    <DropdownMenuItem
+                                      className="flex justify-between text-sm cursor-pointer"
+                                      key={country.code}
+                                      onClick={() => setSelectedDialCode(country.dialCode)}
+                                    >
+                                      <span>{country.name}</span>
+                                      <span className="text-gray-500">{country.dialCode}</span>
+                                    </DropdownMenuItem>
+                                  ))
+                                ) : (
+                                  <div className="text-center text-sm text-gray-500 p-2">
+                                    No countries found
+                                  </div>
+                                )}
+                              </DropdownMenuContent>
+                            </DropdownMenu>
+
+                            {/* Input field for phone number */}
+                            <Input
+                              className="flex-1 border rounded-md p-2"
+                              value={localPhoneNumber} // Show local phone number only
+                              onChange={(e) => setLocalPhoneNumber(e.target.value)}
+                              placeholder="Nomor Telepon"
+                            />
+                          </div>
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    );
+                  }}
+                />
+
 
                 <div className="flex justify-end gap-4">
                   <Button type="button" variant="outline" onClick={onClose}>
                     Batal
                   </Button>
-                  <Button type="submit" className="bg-red-500 text-white">
-                    Simpan
-                  </Button>
+                  {isLoading ? (
+                    <LoadingButton/>
+                  ) : (
+                    <Button type="submit" className="bg-red-500 text-white">
+                      Simpan
+                    </Button>
+                  )}
                 </div>
               </form>
             </Form>
