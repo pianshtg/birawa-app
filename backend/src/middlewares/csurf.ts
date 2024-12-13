ac68777a1e0eeffd7ea474164bb349dab259a42f
Michael Sihotang <sihotangpian@gmail.com>
2024-12-13 15:00:32 +0700

    feat: add csrf middleware

--- backend/src/middlewares/csurf.ts
+++ backend/src/middlewares/csurf.ts
@@ -1,5 +1,5 @@
 import csrf from "csurf";
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 
 declare global {
     namespace Express {
@@ -9,13 +9,25 @@ declare global {
     }
   }
 
-export const csrfProtection = csrf({
+const csrfConfig = csrf({
     cookie: {
         httpOnly: false, // Allow JavaScript access to the CSRF token
         secure: true,
         sameSite: 'strict'
     }
-});
+})
+
+export const csrfProtection = (req: Request, res: Response, next: NextFunction) => {
+  const clientType = req.headers['x-client-type'] // Get client type from headers
+
+  if (clientType === 'mobile') {
+      console.log('Skipping CSRF protection for mobile client.'); // Debug
+      return next() // Skip CSRF protection for mobile clients
+  }
+
+  console.log('Applying CSRF protection for web client.') // Debug
+  csrfConfig(req, res, next) // Apply CSRF protection for web clients
+};
 
 export const csrfToken = (req: Request, res: Response) => {
     res.json({csrfToken: req.csrfToken()})
